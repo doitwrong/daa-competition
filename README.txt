af597f9755fc5fb1cb313580ef54c3b1ad3e8007
doitwrong <kaloyan.katsarov@gmail.com>
2015-04-21 09:57:31 +0300

    first view

--- README.txt
+++ README.txt
@@ -1,1 +1,5 @@
 daa-competition README
+
+
+$VENV/bin/python setup.py develop
+$VENV/bin/pserve development.ini --reload
--- daacompetition/views.py
+++ daacompetition/views.py
@@ -1,6 +1,20 @@
 from pyramid.view import view_config
 
+from pyramid.httpexceptions import (
+    HTTPFound,
+    HTTPNotFound,
+    )
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'daa-competition'}
+
+
+@view_config(route_name='submit', renderer='templates/submittask.pt')
+def submit(request):
+    pagename = 'submit'
+    edit_url = request.route_url('submit', pagename=pagename)
+    content='dasdasdasd'
+    return dict(pagename=pagename, content=content, edit_url=edit_url)
+
+
