90c2fe3803bec4ce15190c24df8c573514dba81d
doitwrong <kaloyan.katsarov@gmail.com>
2015-06-30 16:17:53 +0300

    dsadsa

--- daacompetition/__init__.py
+++ daacompetition/__init__.py
@@ -17,7 +17,7 @@ def main(global_config, **settings):
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
     #config.add_route('home', '/')
-    config.add_route('submit', '/')
+    config.add_route('viewtests', '/')
     #config.add_route('submit', '/submit')
     config.add_route('login', '/login')
     config.add_route('logout', '/logout')
