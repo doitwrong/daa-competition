972ccc43ab8a396c6fc9b45b8dc5d88744b065de
doitwrong <kaloyan.katsarov@gmail.com>
2015-06-30 15:48:28 +0300

    dsadsa

--- daacompetition/tests.py
+++ daacompetition/tests.py
@@ -3,15 +3,27 @@ import unittest
 from pyramid import testing
 
 
-class ViewTests(unittest.TestCase):
+class CommonTests(unittest.TestCase):
+
     def setUp(self):
         self.config = testing.setUp()
 
     def tearDown(self):
         testing.tearDown()
 
-    def test_my_view(self):
-        from .views import my_view
+    def test_login(self):
+        #TODO toya test ne raboti
+
+        #from .views import my_view
+        #request = testing.DummyRequest()
+        #info = my_view(request)
+        #self.assertEqual(info['project'], 'daa-competition')
+        from .views import login
         request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['project'], 'daa-competition')
+        request.params['login'] = 'student'
+        request.params['password'] = 'student'
+
+        #request.route_url()
+        #request.route_url('login') = 'http://localhost:6543/login'
+        print("CCCCCC")
+        print(login(request))
