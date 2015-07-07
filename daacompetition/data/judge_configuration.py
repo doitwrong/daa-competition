bd63b37c895592aed67b16e6435ed3bbd5209e03
doitwrong <kaloyan.katsarov@gmail.com>
2015-07-07 10:10:11 +0300

    dasd

--- daacompetition/data/judge_configuration.py
+++ daacompetition/data/judge_configuration.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 class TimeConfiguration:
     ''' nastroiki na vremeto za saztezanie'''
     start_date = datetime(2015, 7, 7, 7)
-    duration = timedelta(hours=1)
+    duration = timedelta(hours=300)
     expires = start_date + duration
 
 # print(format(start_date, '%Y.%m.%d %H:%M:%S'))
