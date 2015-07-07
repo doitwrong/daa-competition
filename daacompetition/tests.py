252a611a609f1a828a6f0d8e76dd52c76baba661
doitwrong <kaloyan.katsarov@gmail.com>
2015-07-07 08:31:38 +0300

    dobaveni exceptioni

--- daacompetition/tests.py
+++ daacompetition/tests.py
@@ -12,18 +12,18 @@ class CommonTests(unittest.TestCase):
         testing.tearDown()
 
     def test_login(self):
-        #TODO toya test ne raboti
+        # TODO toya test ne raboti
 
-        #from .views import my_view
-        #request = testing.DummyRequest()
-        #info = my_view(request)
-        #self.assertEqual(info['project'], 'daa-competition')
+        # from .views import my_view
+        # request = testing.DummyRequest()
+        # info = my_view(request)
+        # self.assertEqual(info['project'], 'daa-competition')
         from .views import login
         request = testing.DummyRequest()
         request.params['login'] = 'student'
         request.params['password'] = 'student'
 
-        #request.route_url()
-        #request.route_url('login') = 'http://localhost:6543/login'
+        # request.route_url()
+        # request.route_url('login') = 'http://localhost:6543/login'
         print("CCCCCC")
         print(login(request))
