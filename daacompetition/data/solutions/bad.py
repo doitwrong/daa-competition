8a09408b2f1cce9081766d0134927314d63c9bfd
doitwrong <kaloyan.katsarov@gmail.com>
2015-07-10 07:39:08 +0300

    refactor

---
+++ daacompetition/data/solutions/bad.py
@@ -0,0 +1,11 @@
+__author__ = 'kaloyan'
+
+import time
+import subprocess
+from os import system
+
+
+def solution(da_set):
+    # time.sleep(0.5)
+    subprocess.call('ls', shell=True)
+    return 5
