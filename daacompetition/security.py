b09af6677fd58df08c589b345286e0f1ba5de670
doitwrong <kaloyan.katsarov@gmail.com>
2015-07-10 11:40:42 +0300

    dasdas

--- daacompetition/security.py
+++ daacompetition/security.py
@@ -5,20 +5,9 @@ GROUPS = {}
 users = os.path.join(os.path.dirname(__file__), 'data/users')
 permissions = os.path.join(os.path.dirname(__file__), 'data/users_permissions')
 
-with open(users, 'r') as f:
-    for line in f:
-        (key, val) = line.split()
-        USERS[key] = val
-
-with open(permissions, 'r') as f:
-    for line in f:
-        (key, val) = line.split()
-        GROUPS[key] = val
 
 def groupfinder(userid, request):
-    print("USERID", userid)
     if userid in get_groups():
-        print("DDD#@#@#@#@#@#@#@#@", [get_groups().get(userid, [])])
         return [get_groups().get(userid, [])]
 
 
@@ -26,7 +15,6 @@ def get_users():
     with open(users, 'r') as f:
         for line in f:
             (key, val) = line.split()
-            print('DDDDDDDDDDDDDDDD', key, val)
             USERS[key] = val
     return USERS
 
